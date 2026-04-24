2dedf6c9e85f7458dd341d22d3f36cee039e85c3
priyanka thakuri <thakuripriyanka95@gmail.com>
2026-04-25 00:47:02 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { useState } from 'react'
 import Navbar from "./layouts/navbar"
 import Footer from "./layouts/footer"
 import Home from "./pages/home"
 import Project from "./pages/project"
 import ContactUs from "./pages/contactus"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AboutUs from './pages/about';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-   <Router>
+    <Router>
       <Navbar />
-        <Routes>
-          <Route path="/home" element={<Home/>} />
-          <Route path="/about" element={<AboutUs/>} />
-          <Route path="/project" element={<Project/>} />
-          <Route path="/contact" element={<ContactUs/>} />
-          <Route path="/contact" element={<h1>Contact Page</h1>} />
-        </Routes>
-        <Footer/>
-      </Router>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
